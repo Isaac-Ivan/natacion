e5d9838335d3fe8b27c0d3d3e4800210a01db5bf
Isaac Ivan <oscarisaac1504@gmail.com>
2023-07-25 01:05:17 -0600

    Pues aqui esta bro

---
+++ resources/views/landing-natacion/index.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- Barra de navecion inicio-->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-2" href="#">Lorem ipsum dolor</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row">
+        <img class="" src="{{('img/portada.webp')}}" alt="">
+    </div>
+    <!-- <div class="row portada d-flex justify-content-center" >
+        <div class="col-sm-4">
+            <div class="row">
+                <div class="col-sm-6 p-4">
+                    <div class="mb-3">
+                        <label class="form-label">Nombre(s)</label>
+                        <input class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input class="form-control">
+                    </div>
+
+                </div>
+                <div class="col-sm-6 p-4">
+                    <div class="mb-3">
+                        <label class="form-label">Nombre(s)</label>
+                        <input class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div> -->
+
+</body>
+
+<style>
+
+</style>
+
+</html>
